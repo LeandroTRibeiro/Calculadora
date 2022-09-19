c146723b09df063f47562833445a41dbc3043e7a
LeandroTRibeiro <leandrothiago_ribeiro@hotmail.com>
2022-09-19 12:19:27 -0300

    Calculadora1.2

--- script.js
+++ script.js
@@ -1,55 +1,4 @@
-function Calculadora() {
-    this.display = document.querySelector('#display');
-    this.display.focus();
-    
-    this.startClick = () => {
-        document.addEventListener('click', e => {
-            const element = e.target;
-            if (element.classList.contains('btnum')) this.writeIn(element);
-            if (element.classList.contains('btnClear')) this.ClearDis();
-            if (element.classList.contains('btnDel')) this.delDis();
-            if (element.classList.contains('btnIg')) this.calcu();
-        });
-    };
-
-    this.startKey = () => {
-        document.addEventListener('keyup', e => {
-            if (e.keyCode === 13) this.calcu();
-        });
-    };
-
-    this.writeIn = element => {
-        this.display.value += element.innerText;
-        this.display.focus();
-    };
-    this.start = () => { 
-        this.startClick();
-        this.startKey();
-    };
-
-    this.ClearDis = () => this.display.value = ' ';
-
-    this.delDis = () => this.display.value = this.display.value.slice(0, -1);
-
-    this.calcu = () => { 
-        try {
-            const conta = eval(this.display.value);
-            if (!conta && conta !== 0) {
-                alert('Calculo Invalido!');
-                return;
-            }
-
-            this.display.value = conta;
-
-        } catch(e) {
-            alert('Calculo Invalido!');
-            return;
-        }
-    } ;   
-}
-
-const calculadora = new Calculadora();
-calculadora.start();
+// Travamentos de demais teclas
 
 const goodKey = '0123456789+*/)(-.';
 
@@ -70,4 +19,120 @@ var filterInput = function(val) {
 }
 
 display.addEventListener('input', checkInputTel);
+//
+checkDevice();
+// identificação de aparelho 
+function checkDevice() { 
+    if( navigator.userAgent.match(/Android/i)
+    || navigator.userAgent.match(/webOS/i)
+    || navigator.userAgent.match(/iPhone/i)
+    || navigator.userAgent.match(/iPad/i)
+    || navigator.userAgent.match(/iPod/i)
+    || navigator.userAgent.match(/BlackBerry/i)
+    || navigator.userAgent.match(/Windows Phone/i)) {
+        function Calculadora() {
+            this.display = document.querySelector('#display');
+            
+            this.startClick = () => {
+                document.addEventListener('click', e => {
+                    const element = e.target;
+                    if (element.classList.contains('btnum')) this.writeIn(element);
+                    if (element.classList.contains('btnClear')) this.ClearDis();
+                    if (element.classList.contains('btnDel')) this.delDis();
+                    if (element.classList.contains('btnIg')) this.calcu();
+                });
+            };
+        
+            this.startKey = () => {
+                document.addEventListener('keyup', e => {
+                    if (e.keyCode === 13) this.calcu();
+                });
+            };
+        
+            this.writeIn = element => {
+                this.display.value += element.innerText;
+                
+            };
+            this.start = () => { 
+                this.startClick();
+                this.startKey();
+            };
+        
+            this.ClearDis = () => this.display.value = ' ';
+        
+            this.delDis = () => this.display.value = this.display.value.slice(0, -1);
+        
+            this.calcu = () => { 
+                try {
+                    const conta = eval(this.display.value);
+                    if (!conta && conta !== 0) {
+                        alert('Calculo Invalido!');
+                        return;
+                    }
+        
+                    this.display.value = conta;
+        
+                } catch(e) {
+                    alert('Calculo Invalido!');
+                    return;
+                }
+            } ;   
+        }
+        const calculadora = new Calculadora();
+        calculadora.start();
 
+     } else {
+        function Calculadora() {
+            this.display = document.querySelector('#display');
+            this.display.focus();
+            
+            this.startClick = () => {
+                document.addEventListener('click', e => {
+                    const element = e.target;
+                    if (element.classList.contains('btnum')) this.writeIn(element);
+                    if (element.classList.contains('btnClear')) this.ClearDis();
+                    if (element.classList.contains('btnDel')) this.delDis();
+                    if (element.classList.contains('btnIg')) this.calcu();
+                });
+            };
+        
+            this.startKey = () => {
+                document.addEventListener('keyup', e => {
+                    if (e.keyCode === 13) this.calcu();
+                });
+            };
+        
+            this.writeIn = element => {
+                this.display.value += element.innerText;
+                this.display.focus();
+            };
+            this.start = () => { 
+                this.startClick();
+                this.startKey();
+            };
+        
+            this.ClearDis = () => this.display.value = ' ';
+        
+            this.delDis = () => this.display.value = this.display.value.slice(0, -1);
+        
+            this.calcu = () => { 
+                try {
+                    const conta = eval(this.display.value);
+                    if (!conta && conta !== 0) {
+                        alert('Calculo Invalido!');
+                        return;
+                    }
+        
+                    this.display.value = conta;
+        
+                } catch(e) {
+                    alert('Calculo Invalido!');
+                    return;
+                }
+            } ;   
+        }
+        
+        const calculadora = new Calculadora();
+        calculadora.start();
+     }
+   }
